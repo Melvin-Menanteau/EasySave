96a468d1f02cd73a66b3573617110ae6976ad768
Wrench451 <pierre.carteron37@gmail.com>
2024-02-02 11:48:05 +0100

    Création du projet

---
+++ EasySave/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace EasySave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ EasySaveTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EasySaveTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
