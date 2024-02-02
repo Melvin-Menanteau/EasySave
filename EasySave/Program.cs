036a0805f42fee3b73bb17c20e169fec4d208c11
Wrench451 <pierre.carteron37@gmail.com>
2024-02-02 16:27:46 +0100

    Update Program.cs

--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace EasySave
+﻿using System;
+
+namespace EasySave
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("> ");
+                Console.ResetColor();
+
+                var variable = Console.ReadLine();
+
+                if (variable == string.Empty) 
+                {
+                    continue;
+                }
+                else if (variable.StartsWith("cs"))
+                {
+                    Console.WriteLine("Création d'une sauvegarde");
+                }
+                else if (variable.StartsWith("ls"))
+                {
+                    Console.WriteLine("Lancement d'une sauvegarde");
+                }
+            }
         }
     }
 }
