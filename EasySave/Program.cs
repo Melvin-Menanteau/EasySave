2c802739acd670089fe524a212830590f43acb5d
Wrench451 <pierre.carteron37@gmail.com>
2024-02-06 14:13:32 +0100

    Update Program.cs

--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -19,19 +19,6 @@ namespace EasySave
                 {
                     continue;
                 }
-                else if (commande.StartsWith("ls"))
-                {
-                    EasySave easySaveC = new EasySave();
-                    SaveConfiguration saveConfiguration;
-                    saveConfiguration = SaveConfiguration.GetInstance();
-                    List<int> ids = [];
-                    List<Save> saves = saveConfiguration.GetConfiguration();
-                    foreach (Save save in saves)
-                    {
-                        ids.Add(save.Id);
-                    }
-                    easySaveC.LancerSauvegarde(ids);
-                }
                 else
                 {
                     GetMethodFromCommand getMethodFromCommand = new();
