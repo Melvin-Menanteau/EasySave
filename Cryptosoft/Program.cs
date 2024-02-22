8df37f161c685bfca19821e4396d8664dddfe154
Wrench451 <pierre.carteron37@gmail.com>
2024-02-22 09:56:24 +0100

    Add Cryptosoft Project

---
+++ Cryptosoft/Program.cs
@@ -0,0 +1,42 @@
+﻿using CryptoSoft;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Environment.Exit(-1);
+        }
+        else
+        {
+            DateTime StartDateTime = DateTime.Now;
+
+            string InputFilePath = args[0];
+            string OutputFilePath = args[1];
+            string KeyFile = "C:\\Users\\pierr\\source\\repos\\Prosit5\\Prosit5\\Key\\key.txt";
+
+            byte[] message = FileReader.ReadFile(InputFilePath); // Message à chiffrer
+            byte[] key = FileReader.ReadFile(KeyFile); // Clé de chiffrement
+
+            if (key != null)
+            {
+                byte[] EncryptDecryptMessage = XorEncrypt.EncryptDecrypt(message, key);
+
+                FileReader.WriteFile(OutputFilePath, EncryptDecryptMessage);
+            }
+
+            TimeSpan Duration = DateTime.Now - StartDateTime;
+
+            if (Duration > TimeSpan.Zero)
+            {
+                Console.WriteLine(Duration.ToString());
+            }
+            else
+            {
+                Console.WriteLine("ECHEC");
+            }
+        }
+    }
+}
+
---
+++ Cryptosoft/XorEncrypt.cs
@@ -0,0 +1,18 @@
+﻿namespace CryptoSoft
+{
+    internal class XorEncrypt
+    {
+        public static byte[] EncryptDecrypt(byte[] data, byte[] key)
+        {
+            byte[] encryptedData = new byte[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                // Application de l'opération XOR entre chaque octet du fichier et de la clé
+                encryptedData[i] = (byte)(data[i] ^ key[i % key.Length]);
+            }
+
+            return encryptedData;
+        }
+    }
+}
