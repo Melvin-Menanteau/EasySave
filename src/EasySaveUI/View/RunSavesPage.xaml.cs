c9f2b4c68f06399255caf5bd3cc3f61c81ab8a8f
Wrench451 <pierre.carteron37@gmail.com>
2024-02-23 11:29:27 +0100

    Connerie énorme de Mathéo

--- src/EasySaveUI/View/RunSavesPage.xaml.cs
+++ src/EasySaveUI/View/RunSavesPage.xaml.cs
@@ -9,21 +9,3 @@ public partial class RunSavesPage : ContentPage
 
     
 }
-
-public class VotreViewModel : BindableObject
-{
-    private object _emptyView = "Aucune Sauvegarde";
-
-    public object EmptyView
-    {
-        get { return _emptyView; }
-        set
-        {
-            if (_emptyView != value)
-            {
-                _emptyView = value;
-                OnPropertyChanged();
-            }
-        }
-    }
-}
