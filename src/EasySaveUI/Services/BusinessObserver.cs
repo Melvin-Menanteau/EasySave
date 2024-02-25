3242b30f231a457f6253747a0b836b4db3a3f04b
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2024-02-25 15:23:18 +0100

    Create BusinessObserver.cs

---
+++ src/EasySaveUI/Services/BusinessObserver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveUI.Services
+{
+    internal class BusinessObserver
+    {
+        void Observer(string business)
+        {
+            var processes = Process.GetProcessesByName(business);
+            while (true)
+            {
+                processes = Process.GetProcessesByName(business);
+                if (processes.Length > 0)
+                {
+                    using (Mutex mutex = new Mutex(true, "EasySave", out bool createdNew))
+                    {
+                        mutex.WaitOne(); 
+                        while(processes.Length > 0)
+                        {
+                            processes = Process.GetProcessesByName(business);
+                        }
+                        mutex.ReleaseMutex();
+                    }
+                }
+            }
+        }
+    }
+}
