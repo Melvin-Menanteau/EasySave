ac1a4d75ffb7d221bd1cdac4185528a2050d834e
Faugnell <vic.petit98@gmail.com>
2024-02-26 14:42:41 +0100

    modification parametres

--- src/EasySaveUI/GlobalUsing.cs
+++ src/EasySaveUI/GlobalUsing.cs
@@ -3,6 +3,7 @@ global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
 global using EasySaveUI.View;
 global using EasySaveUI;
+
 global using EasySaveUI.Model;
 global using EasySaveUI.ViewModel;
 global using EasySaveUI.Services;
