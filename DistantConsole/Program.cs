c07b7eaae470ebe01d53b3936820649d3e448ae4
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2024-02-26 11:42:51 +0100

    Update Program.cs

--- DistantConsole/Program.cs
+++ DistantConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Sockets;
+using System.Numerics;
 
 namespace DistantConsole
 {
@@ -6,6 +7,8 @@ namespace DistantConsole
     {
         static void Main(string[] args)
         {
+            List<string> saves = new List<string>();
+
             // get input from user
             bool connected = false;
             while(!connected)
@@ -39,11 +42,11 @@ namespace DistantConsole
 
                             for (int i = 0; i < 20; i++)
                             {
-                                if (i < percentage % 20 - 1)
+                                if (i < percentage % 20 || percentage == 100)
                                 {
                                     progress += "=";
                                 }
-                                else if (i == percentage % 20 - 1)
+                                else if (i == percentage % 20)
                                 {
                                     progress += ">";
                                 }
@@ -53,15 +56,26 @@ namespace DistantConsole
                                 }
                             }
                             progress += "] " + percentage + "%";
-                            Console.WriteLine(progress);
-
-
+                            // check if the progress is already in the list
+                            bool found = false;
+                            for (int i = 0; i < saves.Count; i++)
+                            {
+                                if (saves[i].Contains(name))
+                                {
+                                    saves[i] = progress;
+                                    found = true;
+                                }
+                            }
+                            if (!found)
+                            {
+                                saves.Add(progress);
+                            }
+                            Console.Clear();
+                            foreach (string save in saves)
+                            {
+                                Console.WriteLine(save);
+                            }
                         }
-                        else
-                        {
-                            Console.WriteLine(message);
-                        }
-
                         // TODO: ajouter une methode pour mettre en forme le messag recu. 
                     }
                 }
