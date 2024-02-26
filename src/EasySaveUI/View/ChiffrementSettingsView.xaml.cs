4b3afbb412eb35dd9d85c5b613214f1f9f8cb44e
Wrench451 <pierre.carteron37@gmail.com>
2024-02-26 09:35:17 +0100

    Récupère le front dans le code behind

--- src/EasySaveUI/View/ChiffrementSettingsView.xaml.cs
+++ src/EasySaveUI/View/ChiffrementSettingsView.xaml.cs
@@ -1,7 +1,11 @@
+using System.Diagnostics;
+
 namespace EasySaveUI.View;
 
 public partial class ChiffrementSettingsView : ContentView
 {
+    List<string> ExtensionsList = new List<string>();
+
 	public ChiffrementSettingsView()
 	{
 		InitializeComponent();
@@ -9,11 +13,28 @@ public partial class ChiffrementSettingsView : ContentView
 
     private void OnEditorTextChanged(object sender, TextChangedEventArgs e)
     {
+        string EditorText = editor.Text;
 
+        if (EditorText != null)
+        {
+            ExtensionsList = EditorText.Split(";").ToList();
+            ExtensionsList = ExtensionsList.Select(str => str.Replace(" ", string.Empty).Replace(".", string.Empty)).ToList();
+            ExtensionsList = ExtensionsList.Where(str => !string.IsNullOrEmpty(str)).ToList();
+        }
+        
     }
 
     private void OnEditorCompleted(object sender, EventArgs e)
     {
 
+    }
+
+    private void SaveEditorButton_Clicked(object sender, EventArgs e)
+    {
+        foreach(string Extension in ExtensionsList)
+        {
+            Debug.WriteLine(Extension);
+        }
+        
     }
 }
